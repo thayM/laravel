7b66fcd281ebe96b96084133d9c0cff174fb80bf
thayM <thay.contato1@gmail.com>
2024-04-03 18:17:17 -0300

    aula 03/04

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,23 @@
-<h1>Principal</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    
+<ul>
+    <li><a href="{{ route('site.principal') }}">Principal</a></li>
+    <li><a href="{{ route('site.sobreNos') }}">Sobre nós</a></li>
+    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+    <li><a href="{{ route('site.login') }}">Login</a></li>
+    <li><a href="{{ route('app.fornecedores') }}">Fornecedores</a></li>
+    <li><a href="{{ route('app.produtos') }}">Produtos</a></li>
+    <li><a href="{{ route('app.clientes') }}">Clientes</a></li>
+</ul>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.principal');
 
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
+
+Route::get('/login', [App\Http\Controllers\LoginController::class, 'login'])->name('site.login');
+
+// funcção de callback
+Route::prefix('app')->group(function (){
+    Route::get('/clientes', [App\Http\Controllers\ClientesController::class, 'clientes'])->name('app.clientes');
+    Route::get('/fornecedores', [App\Http\Controllers\FornecedoresController::class, 'fornecedores'])->name('app.fornecedores');
+    Route::get('/produtos', [App\Http\Controllers\ProdutosController::class, 'produtos'])->name('app.produtos');
+});
