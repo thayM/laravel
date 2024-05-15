3951ac1a4d2a3a4f3972c7801d72873a918c46ce
thayM <thay.contato1@gmail.com>
2024-05-15 17:31:16 -0300

    aula 15/05/2024

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -9,34 +9,10 @@
     <div class="container">
     <div class="row d-flex justify-content-center">
         <div class="col-6">
-            <form action="{{route('site.contato')}}" method="GET">
-                <div class="mb-3">
-                    <label for="nome" class="form-label">Nome completo</label>
-                    <input type="text" class="form-control" id="nome" name="nome">
-                </div>
-
-                <div class="mb-3">
-                    <label for="nome" class="form-label">E-mail</label>
-                    <input type="text" class="form-control" id="email" name="email">
-                </div>
-
-                <div class="mb-3">
-                    <label for="mensagem" class="form-label">Mensagem</label>
-                    <input type="text" class="form-control" id="email" name="email">
-                </div>
-
-                    <div class="mb-3">
-                        <select name="" id="">
-                            <option selected>Qual o motivo do contato?</option>
-                            <option value="1"></option>
-                            <option value="2"></option>
-                            <option value="3"></option>
-                        </select>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-
-            </form>
+            @component('site.layouts._components.form_contato', ['dark' => 'bg-light'])
+            <p>A nossa equipe analisará o caso e entraremos em contato!</p>
+            <p>Tempo de resposta de 48 horas!</p>
+            @endcomponent
         </div>
     </div>
 </div>
